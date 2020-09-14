51b1eeb4755330595e0477c405967ec3123558ff
Tomaz Vieira <tom.fogaca@gmail.com>
2020-09-14 12:57:04 +0200

    Adds server and workflow

---
+++ webilastik/server/pixel_classification_web_adapter.py
@@ -0,0 +1,141 @@
+from typing import Type, Hashable, Optional, List
+from urllib.parse import urlparse
+from pathlib import Path
+from dataclasses import dataclass
+
+import flask
+
+from ilastik.workflows.pixelClassification.pixel_classification_workflow import (
+    PixelClassificationWorkflow,
+    DataLane,
+    GuiDataSource,
+)
+from ilastik.server.WebContext import WebContext
+from ilastik.classifiers.ilp_pixel_classifier import IlpVigraPixelClassifier
+from ilastik.features.ilp_filter import IlpFilter
+from ilastik.annotations import Annotation
+from ilastik.filesystem import HttpPyFs
+
+from ndstructs.datasource import PrecomputedChunksDataSource
+from ndstructs.utils import to_json_data
+
+
+@dataclass
+class FeatureSpec:
+    name: str
+    scale: float
+    axis_2d: str
+    num_input_channels: int
+
+
+class PixelClassificationWorkflow2WebAdapter:
+    """A class to expose a PixelClassificationWorkflow as a web interface"""
+
+    def __init__(self, web_context: Type[WebContext], workflow: PixelClassificationWorkflow):
+        self.web_context = web_context
+        self.workflow = workflow
+
+    def _drop_classifier(self) -> None:
+        classifier = self.workflow.drop_classifier()
+        if classifier is not None:
+            self.web_context.remove(self.classificer.__class__, self.classifier)
+
+    def _store_classifier(self, classifier: Optional[IlpVigraPixelClassifier]) -> Optional[Hashable]:
+        if classifier is not None:
+            return self.web_context.store(classifier)
+        else:
+            return None
+
+    def add_lane_for_url(self, url: str):
+        if url.startswith("precomputed://"):
+            url = url.lstrip("precomputed://")
+            ds_class = PrecomputedChunksDataSource
+        else:
+            return flask.Response(f"Unsupported Url: {url}", status=400)
+
+        parsed_url = urlparse(url)
+        pathless_url = parsed_url.scheme + "://" + parsed_url.netloc
+        if parsed_url.scheme in ("http", "https"):
+            fs = HttpPyFs(pathless_url)
+        else:
+            return flask.Response(f"Unsupported Url: {url}", status=400)
+
+        datasource = ds_class(Path(parsed_url.path), filesystem=fs)
+        gui_datasource = GuiDataSource(datasource=datasource)
+        lane = DataLane(RawData=gui_datasource)
+        self.workflow.add_lane(lane)
+
+        self.web_context.store(datasource)
+        self.web_context.store(gui_datasource)
+
+        return flask.jsonify(self.web_context.store(lane).to_json_data())
+
+    def add_feature_extractors(self, extractors: List[IlpFilter], updateClassifier: bool = True) -> flask.Response:
+        "Adds feature extractors to workflow, returns uuid of the extractors"
+        self._store_classifier(self.workflow.add_feature_extractors(extractors, updateClassifier))
+        return flask.jsonify([self.web_context.store(extractor).to_json_data() for extractor in extractors])
+
+    def add_ilp_feature_extractors(self, extractor_specs: List[FeatureSpec]) -> flask.Response:
+        extractors = []
+        for ex_spec in extractor_specs:
+            extractor_class = self.web_context.get_class_named(ex_spec.name)
+            extractor = extractor_class.from_ilp_scale(
+                scale=ex_spec.scale, axis_2d=ex_spec.axis_2d, num_input_channels=ex_spec.num_input_channels
+            )
+            extractors.append(extractor)
+        return self.add_feature_extractors(extractors)
+
+    def get_ilp_feature_extractors(self) -> flask.Response:
+        specs: List[FeatureSpec] = []
+        for fe in self.workflow.feature_extractors:
+            specs.append(
+                FeatureSpec(
+                    name=fe.__class__.__name__,
+                    scale=fe.ilp_scale,
+                    axis_2d=fe.axis_2d,
+                    num_input_channels=fe.num_input_channels,
+                )
+            )
+        return flask.jsonify([to_json_data(spec) for spec in specs])
+
+    def clear_feature_extractors(self, updateClassifier: bool = False) -> flask.Response:
+        return self.remove_feature_extractors(self.workflow.feature_extractors[:], updateClassifier=updateClassifier)
+
+    def remove_feature_extractors(self, extractors: List[IlpFilter], updateClassifier: bool = True) -> flask.Response:
+        classifier = self.workflow.remove_feature_extractors(extractors, updateClassifier)
+        classifier_id = self._store_classifier(classifier)
+        for extractor in extractors:
+            self.web_context.remove(IlpFilter, extractor)
+        return flask.jsonify(classifier_id)
+
+    def add_annotations(self, annotations: List[Annotation], updateClassifier: bool = True) -> flask.Response:
+        "Adds annotations to workflow, returns uuid of the annotations"
+        old_num_lanes = len(self.workflow.lanes)
+        classifier = self.workflow.add_annotations(annotations, updateClassifier=updateClassifier)
+        self._store_classifier(classifier)
+        annotation_refs = [self.web_context.store(annotation).to_json_data() for annotation in annotations]
+        for lane in self.workflow.lanes[old_num_lanes:]:
+            self.web_context.store(lane)
+        return flask.jsonify(annotation_refs)
+
+    def remove_annotations(self, annotations: List[Annotation], updateClassifier: bool = True) -> flask.Response:
+        classifier = self.workflow.remove_annotations(annotations, updateClassifier=updateClassifier)
+        classifier_id = self._store_classifier(classifier)
+        return flask.jsonify(classifier_id and classifier_id.to_json_data())
+
+    def get_classifier(self) -> flask.Response:
+        if self.workflow.classifier is None:
+            self.workflow.try_update_pixel_classifier(True)
+        classifier_ref = self._store_classifier(self.workflow.classifier)
+        return flask.jsonify(classifier_ref.to_json_data())
+
+    def ilp_project(self) -> flask.Response:
+        return flask.send_file(
+            self.workflow.ilp_file,
+            mimetype="application/octet-stream",
+            as_attachment=True,
+            attachment_filename="MyProject.ilp",
+        )
+
+    def upload_to_cloud_ilastik(self, cloud_ilastik_token: str, project_name: str) -> flask.Response:
+        return flask.jsonify(self.workflow.upload_to_cloud_ilastik(cloud_ilastik_token, project_name))
---
+++ webilastik/server/servertest.py
@@ -0,0 +1,154 @@
+import requests
+import json
+import numpy
+from ilastik.utility import flatten, unflatten, listify
+from ndstructs import Array5D
+import os
+import time
+
+
+def post(*args, **kwargs):
+    return send("post", *args, **kwargs)
+
+
+def get(*args, **kwargs):
+    return send("get", *args, **kwargs)
+
+
+def send(method: str, *args, **kwargs):
+    resp = getattr(requests, method)(*args, **kwargs)
+    if resp.status_code != 200:
+        raise Exception(resp.text)
+    return resp
+
+
+def get_data(endpoint: str, ref):
+    return get(f"http://localhost:5000/{endpoint}/{ref['object_id']}").json()
+
+
+resp = post("http://localhost:5000/PixelClassificationWorkflow2/", data={})
+workflow_ref = resp.json()
+print("~~~~~~~> Workflow ref: ", workflow_ref)
+
+datasources = get("http://localhost:5000/DataSource/").json()
+print("~~~~~~>>>> datasources:")
+print(json.dumps(datasources, indent=4))
+for ds_data in datasources:
+    if ds_data["name"] == "cropped1.png":
+        data_source_data = ds_data
+        _data_source_id = ds_data["__self__"]["object_id"]
+        break
+print(f"cropped1 datasource id: {_data_source_id}")
+
+precomputed_url = f"precomputed://http://localhost:5000/datasource/{_data_source_id}/data"
+lane_ref = post(
+    f"http://localhost:5000/rpc/add_lane_for_url", data=flatten({"__self__": workflow_ref, "url": precomputed_url})
+).json()
+print(f"~~~~~~~>>> Lane ref:")
+print(json.dumps(lane_ref, indent=4))
+
+
+lane_data = get_data("data_lane", lane_ref)
+print(f"~~~~~~~>>> Lane data:")
+print(json.dumps(lane_data, indent=4))
+
+gui_datasource_data = get_data("gui_data_source", lane_data["RawData"])
+print(f"~~~~~~~>>> GuiDataSource data:")
+print(json.dumps(gui_datasource_data, indent=4))
+
+datasource_data = get_data("data_source", gui_datasource_data["datasource"])
+print(f"~~~~~~~>>> DataSource data:")
+print(json.dumps(datasource_data, indent=4))
+datasource_ref = datasource_data["__self__"]
+
+resp = post(
+    f"http://localhost:5000/rpc/add_annotations",
+    data=flatten(
+        {
+            "__self__": workflow_ref,
+            "annotations": [
+                {
+                    "voxels": [{"x": 140, "y": 150}, {"x": 145, "y": 155}],
+                    "color": {"r": 0, "g": 255, "b": 0},
+                    "raw_data": datasource_ref,
+                },
+                {
+                    "voxels": [{"x": 238, "y": 101}, {"x": 229, "y": 139}],
+                    "color": {"r": 0, "g": 255, "b": 0},
+                    "raw_data": datasource_ref,
+                },
+                {
+                    "voxels": [{"x": 283, "y": 87}, {"x": 288, "y": 92}],
+                    "color": {"r": 255, "g": 0, "b": 0},
+                    "raw_data": datasource_ref,
+                },
+                {
+                    "voxels": [{"x": 274, "y": 168}, {"x": 256, "y": 191}],
+                    "color": {"r": 255, "g": 0, "b": 0},
+                    "raw_data": datasource_ref,
+                },
+            ],
+        }
+    ),
+)
+annot_ids = resp.json()
+print(f"Annotation ids:")
+print(json.dumps(annot_ids, indent=4))
+
+resp = post(
+    f"http://localhost:5000/rpc/add_ilp_feature_extractors",
+    data=flatten(
+        {
+            "__self__": workflow_ref,
+            "extractor_specs": [
+                {"name": "GaussianSmoothing", "scale": 0.3, "axis_2d": "z", "num_input_channels": 3},
+                {"name": "GaussianSmoothing", "scale": 1.0, "axis_2d": "z", "num_input_channels": 3},
+                {"name": "GaussianSmoothing", "scale": 1.6, "axis_2d": "z", "num_input_channels": 3},
+                {"name": "HessianOfGaussianEigenvalues", "scale": 0.7, "axis_2d": "z", "num_input_channels": 3},
+            ],
+        }
+    ),
+)
+feature_ids = resp.json()
+print(f"feature ids:: ")
+print(json.dumps(feature_ids, indent=4))
+
+
+classifier_ref = post(f"http://localhost:5000/rpc/get_classifier", data=flatten({"__self__": workflow_ref})).json()
+print(f"Classifier ref:")
+print(json.dumps(classifier_ref, indent=4))
+
+resp = get(f"http://localhost:5000/predictions/{classifier_ref['object_id']}/neuroglancer_shader")
+print(f"SHADER:\n{resp.text}")
+
+predictions_path = f"predictions/{classifier_ref['object_id']}/{datasource_data['__self__']['object_id']}"
+info = get(f"http://localhost:5000/{predictions_path}/info").json()
+print(f"INFO:\n{json.dumps(info, indent=4)}")
+
+
+binary = get(f"http://localhost:5000/{predictions_path}/data/0-500_0-256_0-1").content
+data = numpy.frombuffer(binary, dtype=numpy.uint8).reshape(2, 256, 500)
+a = Array5D(data, axiskeys="cyx")
+a.show_channels()
+
+ilp_contents = post(f"http://localhost:5000/rpc/ilp_project", data=flatten({"__self__": workflow_ref})).content
+ilp_filename = "/tmp/generated_project.ilp"
+with open(ilp_filename, "wb") as f:
+    f.write(ilp_contents)
+print(f"**********WROTE CONTENTS OF .ilp PROJECT TO {ilp_filename}")
+
+# workflow_data = get(f"http://localhost:5000/PixelClassificationWorkflow2/{workflow_ref}").json()
+# print(json.dumps(workflow_data, indent=4))
+
+resp = post(
+    f"http://localhost:5000/rpc/upload_to_cloud_ilastik",
+    data=flatten(
+        {
+            "__self__": workflow_ref,
+            "cloud_ilastik_token": os.environ["CLOUD_ILASTIK_TOKEN"],
+            "project_name": f"MyTestProject_{time.time()}",
+        }
+    ),
+)
+upload_data = resp.json()
+print(json.dumps(upload_data, indent=4))
