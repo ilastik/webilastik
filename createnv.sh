c408fc0fa60f9e1e2644399443fc06e55dbbdd15
Tomaz Vieira <tom.fogaca@gmail.com>
2020-10-01 11:06:25 +0200

    Better (though still janky) createenv.sh

--- createnv.sh
+++ createnv.sh
@@ -2,8 +2,11 @@
 
 set -e
 set -x
+set -u
 
-conda create -n webilastik -c ilastik-forge -c conda-forge \
+ENVIRONMENT_NAME="webilastik"
+
+conda create -n ${ENVIRONMENT_NAME} -c ilastik-forge -c conda-forge \
     numpy \
     fastfilters \
     fs \
@@ -16,5 +19,17 @@ conda create -n webilastik -c ilastik-forge -c conda-forge \
     flask-cors \
     requests \
     ipython \
-    #dask \
-    #dask-image \
+
+export YELLOW="\e[33m"
+export RED="\e[31m"
+export END_COLOR="\e[0m"
+
+echo -e "${YELLOW}Make sure you have the ${RED}dev${YELLOW} branch of ${RED}ndstructs${YELLOW} in your PYTHONPATH${END_COLOR}"
+
+#git clone https://github.com/ilastik/ndstructs -b dev
+
+#BASE_ENVIRONMENT="$(conda info | grep "base environment" | awk '{print $4}')"
+#ENV_PATH="${BASE_ENVIRONMENT/envs/${ENVIRONMENT_NAME}}"
+#PYTHON="${ENV_PATH}/bin/python"
+
+#$PYTHON -m pip install -e ndstructs/
