fdbcebb923c1ad52ecf0b793be2f5d78425511f7
Tomaz Vieira <tom.fogaca@gmail.com>
2021-02-19 16:50:31 +0100

    Fixes createenv.sh script. Autodownloads ndstructs

--- createnv.sh
+++ createnv.sh
@@ -6,7 +6,7 @@ set -u
 
 ENVIRONMENT_NAME="webilastik"
 
-conda create -n ${ENVIRONMENT_NAME} -c ilastik-forge -c conda-forge \
+conda create -n ${ENVIRONMENT_NAME} -c ilastik-forge -c conda-forge python=3.7 \
     numpy \
     fastfilters \
     fs \
@@ -15,22 +15,17 @@ conda create -n ${ENVIRONMENT_NAME} -c ilastik-forge -c conda-forge \
     scikit-learn \
     h5py \
     typing_extensions \
-    flask \
-    flask-cors \
+    aiohttp \
     requests \
     pytest \
+    python-jose \
     ipython \
 
-export YELLOW="\e[33m"
-export RED="\e[31m"
-export END_COLOR="\e[0m"
+BASE_ENVIRONMENT="$(conda info | grep "base environment" | awk '{print $4}')"
+ENV_PATH="${BASE_ENVIRONMENT}/envs/${ENVIRONMENT_NAME}"
+PYTHON="${ENV_PATH}/bin/python"
 
-echo -e "${YELLOW}Make sure you have the ${RED}dev${YELLOW} branch of ${RED}ndstructs${YELLOW} in your PYTHONPATH${END_COLOR}"
+$PYTHON -m pip install pydevd uwsgi
 
-#git clone https://github.com/ilastik/ndstructs -b dev
-
-#BASE_ENVIRONMENT="$(conda info | grep "base environment" | awk '{print $4}')"
-#ENV_PATH="${BASE_ENVIRONMENT/envs/${ENVIRONMENT_NAME}}"
-#PYTHON="${ENV_PATH}/bin/python"
-
-#$PYTHON -m pip install -e ndstructs/
+git clone https://github.com/ilastik/ndstructs -b dev
+$PYTHON -m pip install -e ndstructs/
