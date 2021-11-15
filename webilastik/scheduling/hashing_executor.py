1302163c3fe1e8641bc5e1041da8d2f1509d8a9a
Tomaz Vieira <tom.fogaca@gmail.com>
2021-11-15 14:42:33 +0100

    [WIP] Refines hashingExecutor type hints

--- webilastik/scheduling/hashing_executor.py
+++ webilastik/scheduling/hashing_executor.py
@@ -1,4 +1,5 @@
-from __future__ import annotations
+# pyright: strict
+
 from concurrent import futures
 from typing import Awaitable, Hashable, Iterator, Optional, TypeVar, Callable
 from concurrent.futures import ProcessPoolExecutor
@@ -13,9 +14,9 @@ OUT = TypeVar("OUT")
 # Does not inherit from concurrent.futures.Executor because the typing there is looser (too many Any's)
 class HashingExecutor:
     """Exports the outputs of an operator created by an upstream applet."""
-    def __init__(self, num_workers: Optional[int] = None):
-        max_workers = num_workers or max(1, multiprocessing.cpu_count() - 1)
-        self._executors = [ProcessPoolExecutor(max_workers=1) for _ in range(max_workers)]
+    def __init__(self, max_workers: Optional[int] = None):
+        self.max_workers = max_workers or max(1, multiprocessing.cpu_count() - 1)
+        self._executors = [ProcessPoolExecutor(max_workers=1) for _ in range(self.max_workers)]
 
     def _get_executor(self, arg: Hashable) -> ProcessPoolExecutor:
         return self._executors[hash(arg) % len(self._executors)]
@@ -28,7 +29,7 @@ class HashingExecutor:
         executor = self._get_executor(arg)
         return loop.run_in_executor(executor, target, arg)
 
-    def shutdown(self, wait=True):
+    def shutdown(self, wait: bool = True):
         for idx, executor in enumerate(self._executors):
             print(f"===> Shutting down executor {idx} from {self}")
             executor.shutdown(wait)
@@ -53,12 +54,12 @@ class HashingExecutor:
                         yield fs.pop().result(end_time - time.monotonic())
             finally:
                 for future in fs:
-                    future.cancel()
+                    _ = future.cancel()
         return result_iterator()
 
     def __enter__(self):
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def __exit__(self, exc_type, exc_val, exc_tb): # type: ignore
         self.shutdown(wait=True)
         return False
