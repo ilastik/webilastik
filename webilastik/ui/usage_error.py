9e92cf0d74f449c440b9b06feff6aecdb389138b
Tomaz Vieira <tom.fogaca@gmail.com>
2022-01-14 17:11:21 +0100

    Adds UsageError.to_json_value

--- webilastik/ui/usage_error.py
+++ webilastik/ui/usage_error.py
@@ -1,4 +1,5 @@
 from typing import Optional
+from ndstructs.utils.json_serializable import JsonObject
 from webilastik.ui.applet import PropagationError, PropagationResult
 
 
@@ -10,3 +11,9 @@ class UsageError(Exception):
         if isinstance(result, PropagationError):
             return UsageError(result.message)
         return None
+
+    def to_json_value(self) -> JsonObject:
+        return {
+            "message": str(self),
+            "__class__": self.__class__.__name__
+        }
