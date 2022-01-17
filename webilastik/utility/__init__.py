69c501174ad8a6e230b89b1b1b87da5e07badcdd
Tomaz Vieira <tom.fogaca@gmail.com>
2022-01-17 19:50:50 +0100

    Adds get_now_string function to utils

--- webilastik/utility/__init__.py
+++ webilastik/utility/__init__.py
@@ -1,2 +1,7 @@
 # pyright: reportUnusedImport=false
 from .flatten import flatten, unflatten, listify
+import datetime
+
+def get_now_string() -> str:
+    now = datetime.datetime.now()
+    return f"{now.year:02}y{now.month:02}m{now.day:02}d__{now.hour:02}h{now.minute:02}m{now.second:02}s"
