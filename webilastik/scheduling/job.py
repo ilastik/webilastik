110bfcbe459b2946ee54f91704f549f30257d2cb
Tomaz Vieira <tom.fogaca@gmail.com>
2022-03-11 14:23:10 +0100

    [WIP] Makes Job's output generic. JobExecutor now queue based

--- webilastik/scheduling/job.py
+++ webilastik/scheduling/job.py
@@ -1,3 +1,4 @@
+import queue
 from typing import Callable, Generic, Iterable, List, Literal, Protocol, Any, Sized, TypeVar
 from collections.abc import Sized
 import threading
@@ -8,6 +9,7 @@ from ndstructs.utils.json_serializable import JsonObject
 from webilastik.utility import PeekableIterator
 
 IN = TypeVar("IN", covariant=True)
+OUT = TypeVar("OUT")
 
 class JobProgressCallback(Protocol):
     def __call__(self, job_id: uuid.UUID, step_index: int) -> Any:
@@ -17,12 +19,12 @@ class JobCompletedCallback(Protocol):
     def __call__(self, job_id: uuid.UUID) -> Any:
         ...
 
-class Job(Generic[IN], Future[None]):
+class Job(Generic[IN, OUT], Future[OUT]):
     def __init__(
         self,
         *,
         name: str,
-        target: Callable[[IN], None],
+        target: Callable[[IN], OUT],
         on_progress: "JobProgressCallback | None" = None,
         on_complete: "JobCompletedCallback | None" = None,
         args: Iterable[IN],
@@ -32,7 +34,8 @@ class Job(Generic[IN], Future[None]):
         self.name = name
         self.target = target
         self.on_progress: "JobProgressCallback | None" = on_progress
-        self.on_complete: "JobCompletedCallback | None" = on_complete
+        if on_complete:
+            self.add_done_callback(lambda _: on_complete(self.uuid))
         self.args: PeekableIterator[IN] = PeekableIterator(args)
         self.num_args: "int | None" = num_args or (len(args) if isinstance(args, Sized) else None)
 
@@ -41,7 +44,7 @@ class Job(Generic[IN], Future[None]):
         self.num_dispatched_steps = 0
         self.lock = threading.Lock()
 
-    def _launch_next_step(self, executor: Executor) -> "Future[None] | None":
+    def _launch_next_step(self, executor: Executor) -> "Future[OUT] | None":
         with self.lock:
             if self.done() or not self.args.has_next():
                 return None
@@ -61,7 +64,7 @@ class Job(Generic[IN], Future[None]):
                 elif future.exception():
                     self.set_exception(future.exception())
                 elif not self.args.has_next() and self.num_dispatched_steps == self.num_completed_steps:
-                    self.set_result(None)
+                    self.set_result(future.result())
             if self.on_progress:
                 self.on_progress(self.uuid, step_index)
 
@@ -92,28 +95,87 @@ class Job(Generic[IN], Future[None]):
                 "error_message": exception and str(exception)
             }
 
+# class JobExecutor:
+#     def __init__(self, executor: Executor, concurrent_job_steps: int) -> None:
+#         self.executor = executor
+#         self.concurrent_job_steps = concurrent_job_steps
+#         self.jobs: "List[Job[Any, Any]]" = []
+#         self.lock = threading.Lock()
+
+#     def _execute_step(self):
+#         with self.lock:
+#             if not self.jobs:
+#                 return
+#             job = self.jobs[0]
+#             future = job._launch_next_step(self.executor)
+#             if future is None:
+#                 _ = self.jobs.pop(0)
+#                 future = self.executor.submit(lambda : None)
+#         future.add_done_callback(lambda _: self._execute_step())
+
+#     def submit(self, job: Job[Any, Any]):
+#         with self.lock:
+#             self.jobs.append(job)
+#             if len(self.jobs) != 1:
+#                 return
+#             for i in range(self.concurrent_job_steps):
+#                 _ = self.executor.submit(lambda : print(f"SUBMISSION TEST {i}"))
+#                 _ = self.executor.submit(self._execute_step)
+
+class _Stop:
+    pass
+
+class _StepToken:
+    pass
+
 class JobExecutor:
     def __init__(self, executor: Executor, concurrent_job_steps: int) -> None:
         self.executor = executor
         self.concurrent_job_steps = concurrent_job_steps
-        self.jobs: "List[Job[Any]]" = []
-        self.lock = threading.Lock()
 
-    def _execute_step(self):
-        with self.lock:
-            if not self.jobs:
-                return
-            job = self.jobs[0]
-            future = job._launch_next_step(self.executor)
-            if future is None:
-                _ = self.jobs.pop(0)
-                future = self.executor.submit(lambda : None)
-        future.add_done_callback(lambda _: self._execute_step())
+        self.job_queue: "queue.Queue[Job[Any, Any] | _Stop]" = queue.Queue()
+        self.step_token_queue: "queue.Queue[_StepToken | _Stop]" = queue.Queue(maxsize=concurrent_job_steps)
+        for _ in range(concurrent_job_steps):
+            self.step_token_queue.put(_StepToken())
 
-    def submit(self, job: Job[Any]):
-        with self.lock:
-            self.jobs.append(job)
-            if len(self.jobs) != 1:
+        self.step_submitter_thread = threading.Thread(
+            group=None, target=self._submit_steps, name=f"JobExecutor_submitter_thread"
+        )
+        self.step_submitter_thread.start()
+
+    def _submit_steps(self):
+        while True:
+            job = self.job_queue.get()
+            if isinstance(job, _Stop):
                 return
-            for _ in range(self.concurrent_job_steps):
-                _ = self.executor.submit(self._execute_step)
+            while True:
+                token = self.step_token_queue.get()
+                if isinstance(token, _Stop):
+                    return
+                future = job._launch_next_step(executor=self.executor)
+                if isinstance(future, Future):
+                    future.add_done_callback(lambda _: self.step_token_queue.task_done() or self.step_token_queue.put(token))
+                else:
+                    self.step_token_queue.put(token)
+                    break
+
+    def _clear_queues(self):
+        queues: "List[queue.Queue[Any]]" = [self.job_queue, self.step_token_queue]
+        for q in queues:
+            try:
+                while True:
+                    q.get_nowait()
+            except queue.Empty:
+                pass
+
+    def shutdown(self):
+        self._clear_queues()
+        self.job_queue.put(_Stop())
+        self.step_token_queue.put(_Stop())
+        self.step_submitter_thread.join()
+
+    def __del__(self):
+        self.shutdown()
+
+    def submit(self, job: Job[Any, Any]):
+        self.job_queue.put(job)
