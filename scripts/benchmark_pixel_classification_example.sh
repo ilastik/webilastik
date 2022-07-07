7c218eef5992b6bcd58533a0b16eaa2eec2f819a
Tomaz Vieira <tom.fogaca@gmail.com>
2022-07-07 19:02:31 +0200

    Adds example on how to use pixel_classification_benchmark.py

---
+++ scripts/benchmark_pixel_classification_example.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+
+set -uxe
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+PROJECT_DIR="$(realpath $SCRIPT_DIR/..)"
+
+
+EXECUTOR_GETTER_IMPL="$PROJECT_DIR/executor_getters/default/"
+# EXECUTOR_GETTER_IMPL="$PROJECT_DIR/executor_getters/process_pool/"
+# EXECUTOR_GETTER_IMPL="$PROJECT_DIR/executor_getters/thread_pool/"
+
+CACHE_IMPL_DIR="$PROJECT_DIR/caching/redis_cache/"
+# CACHE_IMPL_DIR="$PROJECT_DIR/caching/no_cache/"
+# CACHE_IMPL_DIR="$PROJECT_DIR/caching/lru_cache/"
+
+if echo "$CACHE_IMPL_DIR" | grep "redis_cache"; then
+    export REDIS_UNIX_SOCKET_PATH=/var/run/redis/redis-server.sock #adjust as necessary if using redis cache
+    echo -e "FLUSHDB\nQUIT\n" | redis-cli
+fi
+
+export PYTHONPATH="${PROJECT_DIR}:${CACHE_IMPL_DIR}:${EXECUTOR_GETTER_IMPL}${PYTHONPATH+:$PYTHONPATH}"
+
+python "${PROJECT_DIR}/benchmarks/pixel_classification_benchmark.py" --datasource c_cells
+
