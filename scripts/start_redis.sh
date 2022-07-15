a7e8b5c4ae9312f73422f11aaf6795422f92cf13
Tomaz Vieira <tom.fogaca@gmail.com>
2022-07-15 15:04:40 +0200

    Preventes redis from saving in example script

--- scripts/start_redis.sh
+++ scripts/start_redis.sh
@@ -3,13 +3,18 @@
 set -uxe
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+TMP_DIR="${SCRIPT_DIR}/../tests/tmp"
+REDIS_PID_FILE="${TMP_DIR}/redis.pid"
 
 redis-server \
     --port 0 \
     --unixsocket $REDIS_UNIX_SOCKET_PATH \
+    --pidfile $REDIS_PID_FILE \
     --unixsocketperm 777 \
     --daemonize no \
     --maxmemory-policy allkeys-lru \
     --maxmemory 10gb \
-    --dir $SCRIPT_DIR/tests/tmp \
+    --appendonly no \
+    --save "" \
+    --dir $TMP_DIR \
     "$@"
