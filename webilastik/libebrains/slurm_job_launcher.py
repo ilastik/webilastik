f05f142374d0c81379062aa89a88504d33928c1c
Tomaz Vieira <tom.fogaca@gmail.com>
2022-08-16 11:07:32 +0000

    SshJobLauncher: Centralizes all ssh calls in a single method

--- webilastik/libebrains/slurm_job_launcher.py
+++ webilastik/libebrains/slurm_job_launcher.py
@@ -2,7 +2,7 @@
 
 import asyncio
 from enum import Enum
-from typing import ClassVar, NewType, Dict, Mapping, List, Sequence, Set, Literal
+from typing import ClassVar, NewType, Dict, Mapping, List, Sequence, Set, Literal, Tuple
 import uuid
 from pathlib import PurePosixPath
 import json
@@ -66,6 +66,8 @@ RUNNABLE_STATES = set([
 ])
 
 
+Username = NewType("Username", str)
+Hostname = NewType("Hostname", str)
 Minutes = NewType("Minutes", int)
 Seconds = NewType("Seconds", int)
 SlurmJobId = NewType("SlurmJobId", int)
@@ -151,13 +153,14 @@ class SshJobLauncher:
     def __init__(
         self,
         *,
-        user: str,
-        hostname: str,
+        user: Username,
+        hostname: Hostname,
+        login_node_info: "Tuple[Username, Hostname] | None" = None,
         account: str,
         WEBILASTIK_SOURCE_DIR: PurePosixPath,
         CONDA_ENV_DIR: PurePosixPath,
         MODULES_TO_LOAD: Sequence[str],
-        EXECUTOR_GETTER_IMPLEMENTATION: Literal["default", "jusuf"],
+        EXECUTOR_GETTER_IMPLEMENTATION: Literal["default", "jusuf", "cscs"],
         CACHING_IMPLEMENTATION: Literal["lru_cache",  "no_cache",  "redis_cache"],
 
         extra_sbatch_opts: Mapping[str, str] = {},
@@ -166,6 +169,7 @@ class SshJobLauncher:
         self.user = user
         self.hostname = hostname
         self.account = account
+        self.login_node_info = login_node_info
 
         self.WEBILASTIK_SOURCE_DIR = WEBILASTIK_SOURCE_DIR
         self.CONDA_ENV_DIR = CONDA_ENV_DIR
@@ -176,6 +180,33 @@ class SshJobLauncher:
         self.extra_sbatch_opts = extra_sbatch_opts
         self.extra_environment_vars = extra_environment_vars
         super().__init__()
+
+    async def do_ssh(self, *, command: str, command_args: List[str]) -> "str | Exception":
+        login_node_preamble: List[str] = []
+        if self.login_node_info:
+            login_node_preamble = [
+                "ssh", "-oCheckHostIP=no", "-oBatchMode=yes", f"{self.login_node_info[0]}@{self.login_node_info[1]}",
+            ]
+
+        try:
+            process = await asyncio.create_subprocess_exec(
+                *login_node_preamble,
+                "ssh", "-v", "-oBatchMode=yes", f"{self.user}@{self.hostname}",
+                "--",
+                command,
+                *command_args,
+                stdout=asyncio.subprocess.PIPE,
+                stderr=asyncio.subprocess.PIPE,
+            )
+            stdout, stderr = await process.communicate()
+        except Exception as e:
+            return e
+
+        if process.returncode != 0:
+            return Exception(stderr.decode())
+
+        return stdout.decode()
+
 
     async def launch(
         self,
@@ -208,25 +239,18 @@ class SshJobLauncher:
             ])
         }
 
-        try:
-            process = await asyncio.create_subprocess_exec(
-                "ssh", "-v", "-oBatchMode=yes", f"{self.user}@{self.hostname}",
-                "--",
-                "sbatch",
+        output_result = await self.do_ssh(
+            command="sbatch",
+            command_args=[
                 *[f"{key}={value}" for key, value in sbatch_args.items()],
-                f"{self.WEBILASTIK_SOURCE_DIR}/scripts/launch_webilastik_worker__sbatch.sh",
-                stdout=asyncio.subprocess.PIPE,
-                stderr=asyncio.subprocess.PIPE,
-            )
-            stdout, stderr = await process.communicate()
-        except Exception as e:
-            return e
+               f"{self.WEBILASTIK_SOURCE_DIR}/scripts/launch_webilastik_worker__sbatch.sh",
+            ]
+        )
+        if isinstance(output_result, Exception):
+            return output_result
 
-        if process.returncode != 0:
-            return Exception(stderr.decode())
-
-        print(stdout.decode().split())
-        job_id = SlurmJobId(int(stdout.decode().split()[3]))
+        print(output_result.split())
+        job_id = SlurmJobId(int(output_result.split()[3]))
 
         for i in range(5):
             await asyncio.sleep(0.7)
@@ -236,17 +260,10 @@ class SshJobLauncher:
                 return job
         return Exception(f"Could not retrieve job with id {job_id}")
 
-    async def cancel(self, job: SlurmJob):
-        process = await asyncio.create_subprocess_exec(
-            "ssh", "-v", "-oBatchMode=yes", f"{self.user}@{self.hostname}",
-            "--",
-            "scancel", f"{job.job_id}",
-            stdout=asyncio.subprocess.PIPE,
-            stderr=asyncio.subprocess.PIPE,
-        )
-        _stdout, stderr = await process.communicate()
-        if process.returncode != 0:
-            return Exception(stderr.decode())
+    async def cancel(self, job: SlurmJob) -> "Exception | None":
+        result = await self.do_ssh(command="scancel", command_args=[str(job.job_id)])
+        if isinstance(result, Exception):
+            return result
         return None
 
     async def get_current_job_for_user(self, user_info: UserInfo) -> "SlurmJob | None | Exception":
@@ -298,17 +315,11 @@ class SshJobLauncher:
         if state != None and len(state) > 0:
             sacct_params.append(f"--state={','.join(s.value for s in state)}")
 
-        process = await asyncio.create_subprocess_exec(
-            "ssh", "-v", "-oBatchMode=yes", f"{self.user}@{self.hostname}",
-            "--",
-            "sacct", *sacct_params,
-            stdout=asyncio.subprocess.PIPE,
-            stderr=asyncio.subprocess.PIPE,
-        )
-        stdout, stderr = await process.communicate()
-        if process.returncode != 0:
-            return Exception(stderr.decode())
-        payload = ensureJsonObject(json.loads(stdout))
+        output_result = await self.do_ssh(command="sacct", command_args=sacct_params)
+        if isinstance(output_result, Exception):
+            return output_result
+
+        payload = ensureJsonObject(json.loads(output_result))
         raw_jobs = ensureJsonArray(payload.get("jobs"))
         return [
             SlurmJob.from_json_value(raw_job)
@@ -330,8 +341,8 @@ class SshJobLauncher:
 class JusufSshJobLauncher(SshJobLauncher):
     def __init__(self) -> None:
         super().__init__(
-            user="vieira2",
-            hostname="jusuf.fz-juelich.de",
+            user=Username("vieira2"),
+            hostname=Hostname("jusuf.fz-juelich.de"),
             account="icei-hbp-2022-0010",
             WEBILASTIK_SOURCE_DIR=PurePosixPath("/p/project/icei-hbp-2022-0010/source/webilastik"),
             CONDA_ENV_DIR=PurePosixPath("/p/project/icei-hbp-2022-0010/miniconda3/envs/webilastik"),
