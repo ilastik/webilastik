ba7e8aac516957799a2a2304b26fa3d70082789a
Tomaz Vieira <tom.fogaca@gmail.com>
2022-08-23 13:16:31 +0200

    Annotation.__init__: no cute changes to inputs

--- webilastik/annotations/annotation.py
+++ webilastik/annotations/annotation.py
@@ -125,8 +125,8 @@ class Annotation(ScalarData):
     def __init__(
         self, arr: "np.ndarray[Any, Any]", *, axiskeys: str, location: Point5D = Point5D.zero(), raw_data: DataSource
     ):
-        data5d = Array5D(arr.astype(bool), axiskeys=axiskeys, location=location).contracted_to_non_zero()
-        super().__init__(data5d._data, axiskeys=data5d.axiskeys, location=data5d.location)
+        assert arr.dtype == np.dtype(bool)
+        super().__init__(arr, axiskeys=axiskeys, location=location)
         if not raw_data.interval.contains(self.interval):
             raise AnnotationOutOfBounds(annotation_roi=self.interval, raw_data=raw_data)
         self.raw_data = raw_data
