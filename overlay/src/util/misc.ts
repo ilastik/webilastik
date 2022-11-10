393e8f610e431e2dd22d7bcf0b64c9cc21b6a58d
Tomaz Vieira <tom.fogaca@gmail.com>
2022-11-10 16:44:41 +0100

    Fixes getNowString

--- overlay/src/util/misc.ts
+++ overlay/src/util/misc.ts
@@ -401,8 +401,8 @@ export function setValueIfUnfocused(input: HTMLInputElement, value: string){
 
 export function getNowString(): string{
     let now = new Date()
-    let month = now.getMonth().toString().padStart(2, '0')
-    let day = now.getDay().toString().padStart(2, '0')
+    let month = (now.getMonth() + 1).toString().padStart(2, '0')
+    let day = now.getDate().toString().padStart(2, '0')
     let hours = now.getHours().toString().padStart(2, '0')
     let minutes = now.getMinutes().toString().padStart(2, '0')
     let seconds = now.getSeconds().toString().padStart(2, '0')
