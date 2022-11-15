bd65a7ecfbf557600facd5804dfceedccf2e15a6
Tomaz Vieira <tom.fogaca@gmail.com>
2022-11-15 18:41:46 +0100

    Do not allow OsFs unless WEBILASTIK_ALLOW_OSFS is set to 'true'

--- webilastik/filesystem/osfs.py
+++ webilastik/filesystem/osfs.py
@@ -1,3 +1,5 @@
+import os
+
 from fs.osfs import OSFS
 
 from webilastik.filesystem import Filesystem
@@ -6,6 +8,14 @@ from webilastik.server.message_schema import OsfsMessage
 
 
 class OsFs(OSFS, Filesystem):
+    def __init__(self, root_path: str) -> None:
+        if(os.environ.get("WEBILASTIK_ALLOW_OSFS", "false") != "true"):
+            raise Exception("Not allowed to open local filesystem")
+        super().__init__(
+            root_path,
+            expand_vars=False
+        )
+
     def __getstate__(self) -> OsfsMessage:
         return self.to_message()
 
