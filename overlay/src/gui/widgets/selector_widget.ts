2253f143a724b9bcc2c6daf33b2bb04d10fbc1ae
Tomaz Vieira <tom.fogaca@gmail.com>
2022-12-13 13:23:13 +0100

    Removes unused WidgetSelector

--- overlay/src/gui/widgets/selector_widget.ts
+++ overlay/src/gui/widgets/selector_widget.ts
@@ -1,56 +1,6 @@
 import { createElement, createInput, removeElement, uuidv4 } from "../../util/misc";
 import { InputPopupWidget } from "./popup";
 
-export class WidgetSelector<T extends {element: HTMLElement}>{
-    public readonly element: HTMLElement;
-    private selection: T;
-    constructor({options, onSelection, parentElement, radio_name}: {
-        options: Array<T>,
-        onSelection: (selection: T) => void,
-        parentElement: HTMLElement,
-        radio_name: string
-    }){
-        if(options.length == 0){
-            throw `Trying to create selector widget with empty options list`
-        }
-        this.selection = options[0]
-        this.element = createElement({tagName: "form", parentElement, cssClasses: ["ItkSelector"]})
-        let radio_buttons = new Array<HTMLInputElement>();
-        let widget_containers = new Array<HTMLDivElement>();
-        for(let opt of options){
-            const container = createElement({
-                tagName: "div",
-                parentElement: this.element,
-                inlineCss: {display: "grid", gridTemplateRows: "1", gridTemplateColumns: "2"},
-            })
-
-            let widget_container = createElement({
-                tagName: "div", parentElement: container, inlineCss: {gridRow: "1", gridColumn: "2"}
-            })
-            widget_container.appendChild(opt.element)
-            widget_containers.push(widget_container)
-
-            radio_buttons.push(createInput({
-                inputType: "radio",
-                name: radio_name,
-                parentElement: container,
-                inlineCss: {gridRow: "1", gridColumn: "1"},
-                onClick: () => {
-                    widget_containers.forEach(wc => wc.classList.add("ItkGrayedOut"))
-                    widget_container.classList.remove("ItkGrayedOut")
-                    this.selection = opt
-                    onSelection(opt)
-                },
-            }))
-        }
-        radio_buttons[0].click()
-    }
-
-    public getSelection() : T{
-        return this.selection
-    }
-}
-
 export class SelectorOption<T>{
     private _value: T;
     public readonly radio: HTMLInputElement;
