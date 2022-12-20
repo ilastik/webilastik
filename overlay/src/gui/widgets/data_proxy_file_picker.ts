0f097e77697e49ba9daddbc3fe330511589f673f
Tomaz Vieira <tom.fogaca@gmail.com>
2022-12-20 15:32:23 +0100

    [WIP] data proxy file picker: use new style widgets

--- overlay/src/gui/widgets/data_proxy_file_picker.ts
+++ overlay/src/gui/widgets/data_proxy_file_picker.ts
@@ -1,10 +1,13 @@
 import { BucketFs, Session } from "../../client/ilastik";
-import { createElement, createInput, createInputParagraph } from "../../util/misc";
+import { CssClasses } from "../css_classes";
+import { Button } from "./input_widget";
 import { LiveFsTree } from "./live_fs_tree";
 import { ErrorPopupWidget, PopupWidget } from "./popup";
+import { TextInput } from "./value_input_widget";
+import { Div, Label, Paragraph } from "./widget";
 
 export class DataProxyFilePicker{
-    element: HTMLDivElement;
+    element: Div;
 
     constructor(params: {
         parentElement: HTMLElement,
@@ -13,36 +16,42 @@ export class DataProxyFilePicker{
         okButtonValue?: string,
         onCancel?: () => void,
     }){
-        this.element = createElement({tagName: "div", parentElement: params.parentElement})
-        const bucketNameInput = createInputParagraph({
-            inputType: "text", parentElement: this.element, value: "hbp-image-service", label_text: "Bucket name: "
-        })
-        createInputParagraph({inputType: "button", parentElement: this.element, value: "Open file tree", onClick: () => {
-            const bucketName = bucketNameInput.value
-            if(bucketName.length == 0){
-                new ErrorPopupWidget({message: "Please enter a bucket name"})
-                return
-            }
+        let bucketNameInput: TextInput;
+        this.element = new Div({parentElement: params.parentElement, children: [
+            new Paragraph({parentElement: undefined, cssClasses: [CssClasses.ItkInputParagraph], children: [
+                new Label({parentElement: undefined, innerText: "Bucket name: "}),
+                bucketNameInput = new TextInput({parentElement: undefined, value: "hbp-image-service"})
+            ]})
+        ]})
 
-            const popup = new PopupWidget(`Browsing bucket ${bucketName}`)
-            const fs = new BucketFs({bucket_name: bucketName});
-            const fsTreeWidget = new LiveFsTree({
-                fs,
-                parentElement: popup.element,
-                session: params.session,
-            })
-            const buttonsP = createElement({tagName: "p", parentElement: popup.element})
-            createInput({inputType: "button", parentElement: buttonsP, value: params.okButtonValue || "Ok", onClick: () => {
-                popup.destroy()
-                params.onOk(fsTreeWidget)
-            }})
-            createInput({inputType: "button", parentElement: buttonsP, value: "Cancel", onClick: () => {
-                popup.destroy()
-                if(params.onCancel){
-                    params.onCancel()
+        new Paragraph({parentElement: this.element, cssClasses: [CssClasses.ItkInputParagraph], children: [
+            new Button({inputType: "button", text: "Open file tree", parentElement: undefined, onClick: () => {
+                const bucketName = bucketNameInput.value
+                if(!bucketName){
+                    new ErrorPopupWidget({message: "Please enter a bucket name"})
+                    return
                 }
-            }})
 
-        }})
+                const popup = new PopupWidget(`Browsing bucket ${bucketName}`)
+                const fs = new BucketFs({bucket_name: bucketName});
+                const fsTreeWidget = new LiveFsTree({
+                    fs,
+                    parentElement: popup.element,
+                    session: params.session,
+                })
+                new Paragraph({parentElement: popup.element, cssClasses: [CssClasses.ItkInputParagraph], children: [
+                    new Button({inputType: "button", parentElement: undefined, text: params.okButtonValue || "Ok", onClick: () => {
+                        popup.destroy()
+                        params.onOk(fsTreeWidget)
+                    }}),
+                    new Button({inputType: "button", parentElement: undefined, text: "Cancel", onClick: () => {
+                        popup.destroy()
+                        if(params.onCancel){
+                            params.onCancel()
+                        }
+                    }}),
+                ]})
+            }})
+        ]})
     }
 }
