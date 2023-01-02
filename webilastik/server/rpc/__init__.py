b9b7cd13167048eced411b19a25efa2d7e85140c
Tomaz Vieira <tom.fogaca@gmail.com>
2023-01-02 12:48:50 +0100

    [WIP] dto: allow object tag to be different from __class__

--- webilastik/server/rpc/__init__.py
+++ webilastik/server/rpc/__init__.py
@@ -1,5 +1,12 @@
 class DataTransferObject:
-    pass
+    @classmethod
+    def tag_key(cls) -> str:
+        return "__class__"
+
+    @classmethod
+    def tag_value(cls) -> "str | None":
+        return cls.__name__
+
 
 class MessageParsingError(Exception):
     pass
