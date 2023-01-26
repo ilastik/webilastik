e03debb7fe9e485abed9da84881621b7d7fa0af0
Tomaz Vieira <tom.fogaca@gmail.com>
2023-01-26 18:14:05 +0100

    [DRAFT] Config env vars have no distinction between required and optional

--- webilastik/config.py
+++ webilastik/config.py
@@ -3,19 +3,14 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Callable, ClassVar, Generic, Literal, Optional, TypeVar
+from typing import Generic, List, Literal, Optional, TypeVar
 from typing_extensions import Self
-import functools
 
 from cryptography.fernet import Fernet
 from webilastik.libebrains.oidc_client import OidcClient
-from webilastik.libebrains.user_credentials import EbrainsUserCredentials
-from webilastik.libebrains.user_token import UserToken
 
-from webilastik.serialization.json_serialization import parse_typed_json_from_env_var
-from webilastik.utility import Hostname, Minutes, Username, get_env_var
+from webilastik.utility import get_env_var
 from webilastik.utility.url import Url
-from webilastik.server.rpc.dto import WorkflowConfigDto, SessionAllocatorConfigDto
 
 
 T = TypeVar("T")
@@ -25,8 +20,28 @@ class Config(ABC, Generic[T]):
 
     @classmethod
     @abstractmethod
-    def try_parse(cls, value: str) -> "Self | Exception":
+    def try_parse(cls, value: str) -> "T | Exception":
         pass
+
+    # @functools.lru_cache
+    @classmethod
+    def try_get(cls) -> "Self | None | Exception":
+        value = get_env_var(
+            var_name=cls.__name__,
+            parser=cls.try_parse,
+            default=None,
+        )
+        if isinstance(value, (type(None), Exception)):
+            return value
+        return cls(value)
+
+    @classmethod
+    def require(cls) -> "Self | Exception":
+        value = cls.try_get()
+        if value is None:
+            return Exception(f"Env var {cls.__name__} was not set")
+        return value
+
 
     @abstractmethod
     def to_bash_value(self) -> str:
@@ -35,91 +50,56 @@ class Config(ABC, Generic[T]):
     def to_bash_export(self) -> str:
         return f"export {self.__class__.__name__}={self.to_bash_value()}"
 
-class OptionalConfig(Config["T | None"]):
-    # @functools.lru_cache
-    @classmethod
-    def try_get(cls) -> "Self | Exception":
-        return get_env_var(
-            var_name=cls.__name__,
-            parser=cls.try_parse,
-            default=cls(None),
-        )
 
+class BoolConfig(Config[bool]):
     @classmethod
-    def require_value(cls) -> "T | Exception":
-        instance = cls.try_get()
-        if isinstance(instance, Exception):
-            return instance
-        if instance.value is None:
-            return Exception(f"Environment variable {cls.__name__} was not set")
-        return instance.value
-
-class RequiredConfig(Config[T]):
-    # @functools.lru_cache
-    @classmethod
-    def try_get(cls) -> "Self | Exception":
-        return get_env_var(
-            var_name=cls.__name__,
-            parser=cls.try_parse,
-        )
-
-class BoolConfig(RequiredConfig[bool]):
-    @classmethod
-    def try_parse(cls, value: str) -> "Self | Exception":
+    def try_parse(cls, value: str) -> "bool | Exception":
         if value.lower() in ("false", "0"):
-            return cls(False)
+            return False
         if value.lower() in ("true", "1"):
-            return cls(True)
+            return True
         return Exception(f"Bad value for {cls.__name__}: {value}")
 
     def to_bash_value(self) -> Literal["true", "false"]:
         return "true" if self.value else "false"
 
-class IntConfig(RequiredConfig[int]):
+class IntConfig(Config[int]):
     @classmethod
-    def try_parse(cls, value: str) -> "Self | Exception":
+    def try_parse(cls, value: str) -> "int | Exception":
         try:
-            return cls(int(value))
+            return int(value)
         except Exception as e:
             return e
 
     def to_bash_value(self) -> str:
         return str(self.value)
 
-class PathConfig(RequiredConfig[Path]):
+class PathConfig(Config[Path]):
     @classmethod
-    def try_parse(cls, value: str) -> "Self | Exception":
-        return cls(Path(value))
+    def try_parse(cls, value: str) -> "Path | Exception":
+        return Path(value)
 
     def to_bash_value(self) -> str:
         return str(self.value)
 
-class StringConfig(RequiredConfig[str]):
+class StringConfig(Config[str]):
     @classmethod
-    def try_parse(cls, value: str) -> "Self | Exception":
-        return cls(value)
+    def try_parse(cls, value: str) -> "str | Exception":
+        return value
 
     def to_bash_value(self) -> str:
         return self.value
 
-class UrlConfig(RequiredConfig[Url]):
+class UrlConfig(Config[Url]):
     @classmethod
-    def try_parse(cls, value: str) -> "Self | Exception":
+    def try_parse(cls, value: str) -> "Url | Exception":
         parsed = Url.parse(value)
         if parsed is None:
             return Exception(f"Bad url value for {cls.__name__}: {value}")
-        return cls(parsed)
+        return parsed
 
     def to_bash_value(self) -> str:
         return self.value.raw
-
-class OptionalStringConfig(OptionalConfig["str"]):
-    @classmethod
-    def try_parse(cls, value: str) -> "Self | Exception":
-        return cls(value) if len(value) > 0 else cls(None)
-
-    def to_bash_value(self) -> str:
-        return "" if self.value is None else self.value
 
 
 
@@ -130,95 +110,122 @@ class WEBILASTIK_ALLOW_LOCAL_COMPUTE_SESSIONS(BoolConfig):
 
 
 
-class EBRAINS_OIDC_CLIENT_ID(OptionalStringConfig):
+class EBRAINS_OIDC_CLIENT_ID(StringConfig):
     pass
-class EBRAINS_OIDC_CLIENT_SECRET(OptionalStringConfig):
+class EBRAINS_OIDC_CLIENT_SECRET(StringConfig):
     pass
-@dataclass
 class EbrainsOidcClientConfig:
-    value: "OidcClient | None"
+    def __init__(
+        self,
+        *,
+        client_id: EBRAINS_OIDC_CLIENT_ID,
+        client_secret: EBRAINS_OIDC_CLIENT_SECRET,
+    ):
+        super().__init__()
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.oidc_client = OidcClient(client_id=client_id.value, client_secret=client_secret.value)
 
     @classmethod
-    def try_get(cls) -> "Self | Exception":
-        client_id_config_result = EBRAINS_OIDC_CLIENT_ID.try_get()
-        if isinstance(client_id_config_result, Exception):
-            return client_id_config_result
-        if client_id_config_result.value is None:
-            return cls(None)
+    def try_get(cls) -> "EbrainsOidcClientConfig | None |  Exception":
+        client_id_result = EBRAINS_OIDC_CLIENT_ID.try_get()
+        if isinstance(client_id_result, (Exception, type(None))):
+            return client_id_result
 
-        client_secret_result = EBRAINS_OIDC_CLIENT_SECRET.require_value()
+        client_secret_result = EBRAINS_OIDC_CLIENT_SECRET.try_get()
         if isinstance(client_secret_result, Exception):
             return client_secret_result
+        if isinstance(client_secret_result, type(None)):
+            return Exception(f"{EBRAINS_OIDC_CLIENT_ID.__name__} is set but {EBRAINS_OIDC_CLIENT_SECRET.__name__} is not")
 
-        return cls(value=OidcClient(client_id=client_id_config_result.value, client_secret=client_secret_result))
+        return EbrainsOidcClientConfig(client_id=client_id_result, client_secret=client_secret_result)
 
     @classmethod
-    def require_value(cls) -> "OidcClient | Exception":
-        client_id_result = EBRAINS_OIDC_CLIENT_ID.require_value()
+    def require(cls) -> "Self | Exception":
+        client_id_result = EBRAINS_OIDC_CLIENT_ID.require()
         if isinstance(client_id_result, Exception):
             return client_id_result
 
-        client_secret_result = EBRAINS_OIDC_CLIENT_SECRET.require_value()
+        client_secret_result = EBRAINS_OIDC_CLIENT_SECRET.require()
         if isinstance(client_secret_result, Exception):
             return client_secret_result
 
-        return OidcClient(client_id=client_id_result, client_secret=client_secret_result)
+        return EbrainsOidcClientConfig(client_id=client_id_result, client_secret=client_secret_result)
+
+    def to_bash_exports(self) -> List[str]:
+        return [
+            self.client_id.to_bash_export(),
+            self.client_secret.to_bash_export(),
+        ]
 
 
-class EBRAINS_USER_ACCESS_TOKEN(OptionalStringConfig):
+class EBRAINS_USER_ACCESS_TOKEN(StringConfig):
     pass
-class EBRAINS_USER_REFRESH_TOKEN(OptionalStringConfig):
+class EBRAINS_USER_REFRESH_TOKEN(StringConfig):
     pass
-@dataclass
 class EbrainsUserCredentialsConfig:
-    value: "EbrainsUserCredentials | None"
+    def __init__(
+        self,
+        *,
+        user_access_token: EBRAINS_USER_ACCESS_TOKEN,
+        user_refresh_token: Optional[EBRAINS_USER_REFRESH_TOKEN],
+        oidc_client: Optional[EbrainsOidcClientConfig],
+    ):
+        super().__init__()
+        self.user_access_token = user_access_token
+        self.user_refresh_token = user_refresh_token
+        self.oidc_client = oidc_client
+
+    def to_bash_exports(self) -> List[str]:
+        out = [self.user_access_token.to_bash_export()]
+        if self.user_refresh_token:
+            out.append(self.user_access_token.to_bash_export())
+        if self.oidc_client:
+            out += self.oidc_client.to_bash_exports()
+        return out
 
     @classmethod
-    def try_get(cls) -> "Self | Exception":
+    def try_get(cls) -> "Self | None | Exception":
         user_access_token_result = EBRAINS_USER_ACCESS_TOKEN.try_get()
         if isinstance(user_access_token_result, Exception):
             return user_access_token_result
-        if user_access_token_result.value is None:
-            return cls(value=None)
 
-        user_refresh_token_config_result = EBRAINS_USER_REFRESH_TOKEN.try_get()
-        if isinstance(user_refresh_token_config_result, Exception):
-            return user_refresh_token_config_result
+        user_refresh_token_result = EBRAINS_USER_REFRESH_TOKEN.try_get()
+        if isinstance(user_refresh_token_result, Exception):
+            return user_refresh_token_result
 
-        oid_client_config_result = EbrainsOidcClientConfig.try_get()
-        if isinstance(oid_client_config_result, Exception):
-            return oid_client_config_result
+        oidc_client_result = EbrainsOidcClientConfig.try_get()
+        if isinstance(oidc_client_result, Exception):
+            return oidc_client_result
 
-        return cls(EbrainsUserCredentials(
-            user_token=UserToken(
-                access_token=user_access_token_result.value,
-                refresh_token=user_refresh_token_config_result.value,
-            ),
-            oidc_client=oid_client_config_result.value,
-        ))
+        if user_access_token_result is None:
+            return None
+
+        return EbrainsUserCredentialsConfig(
+            user_access_token=user_access_token_result,
+            user_refresh_token=user_refresh_token_result,
+            oidc_client=oidc_client_result,
+        )
 
     @classmethod
-    def require_value(cls) -> "EbrainsUserCredentials | Exception":
-        user_access_token_result = EBRAINS_USER_ACCESS_TOKEN.require_value()
+    def require(cls) -> "Self | Exception":
+        user_access_token_result = EBRAINS_USER_ACCESS_TOKEN.require()
         if isinstance(user_access_token_result, Exception):
             return user_access_token_result
 
-        user_refresh_token_config_result = EBRAINS_USER_REFRESH_TOKEN.try_get()
-        if isinstance(user_refresh_token_config_result, Exception):
-            return user_refresh_token_config_result
+        user_refresh_token_result = EBRAINS_USER_REFRESH_TOKEN.try_get()
+        if isinstance(user_refresh_token_result, Exception):
+            return user_refresh_token_result
 
-        oid_client_config_result = EbrainsOidcClientConfig.try_get()
-        if isinstance(oid_client_config_result, Exception):
-            return oid_client_config_result
+        oidc_client_result = EbrainsOidcClientConfig.try_get()
+        if isinstance(oidc_client_result, Exception):
+            return oidc_client_result
 
-        return EbrainsUserCredentials(
-            user_token=UserToken(
-                access_token=user_access_token_result,
-                refresh_token=user_refresh_token_config_result.value,
-            ),
-            oidc_client=oid_client_config_result.value,
+        return EbrainsUserCredentialsConfig(
+            user_access_token=user_access_token_result,
+            user_refresh_token=user_refresh_token_result,
+            oidc_client=oidc_client_result,
         )
-
 
 class WEBILASTIK_B64_FERNET_KEY(StringConfig):
     pass
@@ -228,26 +235,35 @@ class WEBILASTIK_EXTERNAL_URL(UrlConfig):
 class SessionAllocatorConfig:
     allow_local_fs: WEBILASTIK_ALLOW_LOCAL_FS
     allow_local_compute_sessions: WEBILASTIK_ALLOW_LOCAL_COMPUTE_SESSIONS
-    ebrains_oidc_client: OidcClient
-    b64_fernet_key: str
+    ebrains_oidc_client: EbrainsOidcClientConfig
+    b64_fernet_key: WEBILASTIK_B64_FERNET_KEY
     fernet: Fernet
-    external_url: Url
+    external_url: WEBILASTIK_EXTERNAL_URL
+
+    def to_bash_exports(self) -> List[str]:
+        return [
+            self.allow_local_fs.to_bash_export(),
+            self.allow_local_compute_sessions.to_bash_export(),
+            *self.ebrains_oidc_client.to_bash_exports(),
+            self.b64_fernet_key.to_bash_export(),
+            self.external_url.to_bash_export(),
+        ]
 
     @classmethod
-    def try_get(cls) -> "Self | Exception":
-        allow_local_fs_config_result = WEBILASTIK_ALLOW_LOCAL_FS.try_get()
+    def require(cls) -> "Self | Exception":
+        allow_local_fs_config_result = WEBILASTIK_ALLOW_LOCAL_FS.require()
         if isinstance(allow_local_fs_config_result, Exception):
             return allow_local_fs_config_result
 
-        allow_local_compute_sessions_config_result = WEBILASTIK_ALLOW_LOCAL_COMPUTE_SESSIONS.try_get()
+        allow_local_compute_sessions_config_result = WEBILASTIK_ALLOW_LOCAL_COMPUTE_SESSIONS.require()
         if isinstance(allow_local_compute_sessions_config_result, Exception):
             return allow_local_compute_sessions_config_result
 
-        ebrains_oidc_client_result = EbrainsOidcClientConfig.require_value()
+        ebrains_oidc_client_result = EbrainsOidcClientConfig.require()
         if isinstance(ebrains_oidc_client_result, Exception):
             return ebrains_oidc_client_result
 
-        b64_fernet_key_result = WEBILASTIK_B64_FERNET_KEY.try_get()
+        b64_fernet_key_result = WEBILASTIK_B64_FERNET_KEY.require()
         if isinstance(b64_fernet_key_result, Exception):
             return b64_fernet_key_result
         try:
@@ -255,17 +271,17 @@ class SessionAllocatorConfig:
         except Exception as e:
             return e
 
-        external_url_result = WEBILASTIK_EXTERNAL_URL.try_get()
+        external_url_result = WEBILASTIK_EXTERNAL_URL.require()
         if isinstance(external_url_result, Exception):
             return external_url_result
 
         return SessionAllocatorConfig(
-            allow_local_fs=allow_local_fs_config_result.value,
-            allow_local_compute_sessions=allow_local_compute_sessions_config_result.value,
+            allow_local_fs=allow_local_fs_config_result,
+            allow_local_compute_sessions=allow_local_compute_sessions_config_result,
             ebrains_oidc_client=ebrains_oidc_client_result,
-            b64_fernet_key=b64_fernet_key_result.value,
+            b64_fernet_key=b64_fernet_key_result,
             fernet=fernet,
-            external_url=external_url_result.value
+            external_url=external_url_result,
         )
 
 class WEBILASTIK_WORKFLOW_MAX_DURATION_MINUTES(IntConfig):
@@ -283,7 +299,7 @@ class WEBILASTIK_WORKFLOW_SESSION_ALLOCATOR_SOCKET_PATH(PathConfig):
 @dataclass
 class WorkflowConfig:
     allow_local_fs: WEBILASTIK_ALLOW_LOCAL_FS
-    ebrains_user_credentials: EbrainsUserCredentialsConfig
+    ebrains_user_credentials: Optional[EbrainsUserCredentialsConfig]
     max_duration_minutes: WEBILASTIK_WORKFLOW_MAX_DURATION_MINUTES
     listen_socket: WEBILASTIK_WORKFLOW_LISTEN_SOCKET
     session_url: WEBILASTIK_WORKFLOW_SESSION_URL
@@ -293,8 +309,8 @@ class WorkflowConfig:
     session_allocator_socket_path: WEBILASTIK_WORKFLOW_SESSION_ALLOCATOR_SOCKET_PATH
 
     @classmethod
-    def try_get(cls) -> "WorkflowConfig | Exception":
-        allow_local_fs_result = WEBILASTIK_ALLOW_LOCAL_FS.try_get()
+    def require(cls) -> "WorkflowConfig | Exception":
+        allow_local_fs_result = WEBILASTIK_ALLOW_LOCAL_FS.require()
         if isinstance(allow_local_fs_result, Exception):
             return allow_local_fs_result
 
@@ -302,27 +318,27 @@ class WorkflowConfig:
         if isinstance(ebrains_user_credentials_result, Exception):
             return ebrains_user_credentials_result
 
-        max_duration_minutes_result = WEBILASTIK_WORKFLOW_MAX_DURATION_MINUTES.try_get()
+        max_duration_minutes_result = WEBILASTIK_WORKFLOW_MAX_DURATION_MINUTES.require()
         if isinstance(max_duration_minutes_result, Exception):
             return max_duration_minutes_result
 
-        listen_socket_result = WEBILASTIK_WORKFLOW_LISTEN_SOCKET.try_get()
+        listen_socket_result = WEBILASTIK_WORKFLOW_LISTEN_SOCKET.require()
         if isinstance(listen_socket_result, Exception):
             return listen_socket_result
 
-        session_url_result = WEBILASTIK_WORKFLOW_SESSION_URL.try_get()
+        session_url_result = WEBILASTIK_WORKFLOW_SESSION_URL.require()
         if isinstance(session_url_result, Exception):
             return session_url_result
 
-        session_alocator_host_result = WEBILASTIK_WORKFLOW_SESSION_ALLOCATOR_HOST.try_get()
+        session_alocator_host_result = WEBILASTIK_WORKFLOW_SESSION_ALLOCATOR_HOST.require()
         if isinstance(session_alocator_host_result, Exception):
             return session_alocator_host_result
 
-        session_allocator_username_result = WEBILASTIK_WORKFLOW_SESSION_ALLOCATOR_USERNAME.try_get()
+        session_allocator_username_result = WEBILASTIK_WORKFLOW_SESSION_ALLOCATOR_USERNAME.require()
         if isinstance(session_allocator_username_result, Exception):
             return session_allocator_username_result
 
-        session_allocator_socket_path_result = WEBILASTIK_WORKFLOW_SESSION_ALLOCATOR_SOCKET_PATH.try_get()
+        session_allocator_socket_path_result = WEBILASTIK_WORKFLOW_SESSION_ALLOCATOR_SOCKET_PATH.require()
         if isinstance(session_allocator_socket_path_result, Exception):
             return session_allocator_socket_path_result
 
@@ -336,4 +352,16 @@ class WorkflowConfig:
             session_allocator_username=session_allocator_username_result,
             session_allocator_socket_path=session_allocator_socket_path_result,
         )
+
+    def to_bash_exports(self) -> List[str]:
+        return [
+            self.allow_local_fs.to_bash_export(),
+            *([] if not self.ebrains_user_credentials else self.ebrains_user_credentials.to_bash_exports()),
+            self.max_duration_minutes.to_bash_export(),
+            self.listen_socket.to_bash_export(),
+            self.session_url.to_bash_export(),
+            self.session_allocator_host.to_bash_export(),
+            self.session_allocator_username.to_bash_export(),
+            self.session_allocator_socket_path.to_bash_export(),
+        ]
 
