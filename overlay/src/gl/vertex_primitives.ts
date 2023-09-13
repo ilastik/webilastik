0abff578b17ad0b6fafab461c41c3864b08808af
Tomaz Vieira <tom.fogaca@gmail.com>
2023-09-13 13:48:37 +0200

    [WIP] Removes unused Triangle class

--- overlay/src/gl/vertex_primitives.ts
+++ overlay/src/gl/vertex_primitives.ts
@@ -18,65 +18,6 @@ export class VertexArray{
     }
 }
 
-function getNormal(out: vec3, verts: Float32Array, vertexOrder: FrontFace = FrontFace.CCW): vec3{
-    if(verts.length != 3*3){
-        throw `Bad verts array length: ${verts.length}`
-    }
-    let v0 = verts.subarray(0, 3)
-    let v1 = verts.subarray(3, 6)
-    let v2 = verts.subarray(6, 9)
-
-    let a = vec3.create(); vec3.sub(a, v1, v0)
-    let b = vec3.create(); vec3.sub(a, v2, v0)
-    if(vertexOrder == FrontFace.CCW){
-        vec3.cross(out, a, b)
-    }else{
-        vec3.cross(out, b, a)
-    }
-    vec3.normalize(out, out)
-    return out
-}
-
-export class Triangle extends VertexArray{
-    //FIXME: allow cw verts?
-    public readonly vertexOrder: FrontFace
-    constructor(data: Float32Array, vertexOrder: FrontFace = FrontFace.CCW){
-        if(data.length != 3 * 3){
-            throw `Expected triangle buffer to have 9 floats and not ${data.length}`
-        }
-        super(data)
-        this.vertexOrder = vertexOrder
-    }
-
-    public static fromVerts(p0: vec3, p1: vec3, p2: vec3, frontFace: FrontFace = FrontFace.CCW): Triangle{
-        return new Triangle(new Float32Array([...p0, ...p1, ...p2]), frontFace)
-    }
-
-    public get p0(): vec3{
-        return this.getVertRef(0)
-    }
-
-    public get p1(): vec3{
-        return this.getVertRef(1)
-    }
-
-    public get p2(): vec3{
-        return this.getVertRef(2)
-    }
-
-    public move(delta: vec3) : Triangle{
-        vec3.add(this.p0, this.p0, delta)
-        vec3.add(this.p1, this.p1, delta)
-        vec3.add(this.p2, this.p2, delta)
-        return this
-    }
-
-    public get_normal() : vec3{
-        let normal = vec3.create();
-        return getNormal(normal, this.data, this.vertexOrder)
-    }
-}
-
 export abstract class VertexPrimitive extends VertexArray{
     private positionsBuffer?: VecAttributeBuffer<3, Float32Array>
 
