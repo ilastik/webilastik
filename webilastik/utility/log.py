1dc3123f077569399cc764f2320730c2c4e02341
Tomaz Vieira <tom.fogaca@gmail.com>
2023-09-18 08:58:22 +0200

    Formats escape codes in Logger for ease of reading

--- webilastik/utility/log.py
+++ webilastik/utility/log.py
@@ -4,10 +4,10 @@ import sys
 
 class Logger:
     debug_escape = "\033[32m" if sys.stderr.isatty()  else ""
-    info_escape = "\033[34m" if sys.stderr.isatty()  else ""
-    warn_escape = "\033[33m" if sys.stderr.isatty()  else ""
+    info_escape =  "\033[34m" if sys.stderr.isatty()  else ""
+    warn_escape =  "\033[33m" if sys.stderr.isatty()  else ""
     error_escape = "\033[31m" if sys.stderr.isatty()  else ""
-    end_escape = "\033[0m" if sys.stderr.isatty()  else ""
+    end_escape =   "\033[0m"  if sys.stderr.isatty()  else ""
 
     def debug(self, message: str):
         print(f"{self.debug_escape}[DEBUG]{message}{self.end_escape}", file=sys.stderr)
